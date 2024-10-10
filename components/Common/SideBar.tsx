d802d42c91c1270abcf58103ec3ac10ddaeb074f
rvif <ravijeeeetsharma4444@gmail.com>
2024-10-11 01:33:16 +0530

    fixed some styles

--- components/Common/SideBar.tsx
+++ components/Common/SideBar.tsx
@@ -24,21 +24,20 @@ const SideBar = ({ user }: SideBarProps) => {
   };
 
   return (
-    <div className="hidden fixed  inset-y-0 left-0 sm:block w-[5rem] bg-white rounded-lg overflow-hidden dark:bg-black dark:border-r">
+    <div className="hidden fixed inset-y-0 left-0 sm:block w-[5rem] bg-white rounded-lg overflow-hidden dark:bg-black dark:border-r">
       <div className="flex flex-col items-center justify-between h-full">
-        {/* TOP PART  */}
-        <div>
-          <div className="my-8">
-            <Image src="/logo.png" width={50} height={50} alt="logo" />
-          </div>
-
-          <nav className="flex flex-col items-center px-3 overflow-y-auto">
-            {user?.role === "ADMIN" && adminIconsRouter()}
-            {user?.role === "USER" && userIconsRouter()}
-            {user?.role === "MODERATOR" && moderatorIconsRouter()}
-          </nav>
+        {/* TOP PART */}
+        <div className="flex flex-col items-center justify-center my-8">
+          <Image src="/logo.png" width={50} height={50} alt="logo" />
         </div>
-        {/* BOTTOM PART  */}
+
+        <nav className="flex flex-col items-center px-3 overflow-y-auto">
+          {user?.role === "ADMIN" && adminIconsRouter()}
+          {user?.role === "USER" && userIconsRouter()}
+          {user?.role === "MODERATOR" && moderatorIconsRouter()}
+        </nav>
+
+        {/* BOTTOM PART */}
         <div className="flex flex-col items-center space-y-6 my-8">
           <LogoutBtn />
           <ToggleDarkLight />
