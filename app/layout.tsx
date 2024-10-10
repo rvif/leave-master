a42839b045d83666aed6967836498138f94710ea
rvif <ravijeeeetsharma4444@gmail.com>
2024-10-10 20:25:53 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Providers from "./Provider";
+
+export const metadata: Metadata = {
+  title: "LeaveMaster",
+  description: "...",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="">
+        <Providers
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
