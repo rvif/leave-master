454c4b259f083f6949bae89b9e0876bb1922ff85
rvif <ravijeeeetsharma4444@gmail.com>
2024-10-11 01:55:05 +0530

    idk

--- components/Common/SideBar.tsx
+++ components/Common/SideBar.tsx
@@ -39,7 +39,6 @@ const SideBar = ({ user }: SideBarProps) => {
 
         {/* BOTTOM PART */}
         <div className="flex flex-col items-center space-y-6 my-8">
-          <LogoutBtn />
           <ToggleDarkLight />
         </div>
       </div>
